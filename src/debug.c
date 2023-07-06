f1619dcb52289cb18025f3a9778d9ae18f7aeee9
plu7o <skankhunt4ort2wo@gmail.com>
2023-07-06 03:16:46 +0200

    Update 1: implemented vm, scanner and setup for compiler

--- src/debug.c
+++ src/debug.c
@@ -13,7 +13,7 @@ void disassembleChunk(Chunk *chunk, const char *name) {
 
 static int constantInstruction(const char *name, Chunk *chunk, int offset) {
   uint8_t constant = chunk->code[offset + 1];
-  printf("%-16s %4d '", name, constant);
+  printf("%-12s --> %4d '", name, constant);
   printValue(chunk->constants.values[constant]);
   printf("'\n");
   return offset + 2;
@@ -25,20 +25,29 @@ static int simpleInstruction(const char *name, int offset) {
 }
 
 int disassembleInstruction(Chunk *chunk, int offset) {
-  printf("%04d ", offset);
-  if (offset > 0 &&
-      chunk->lines[offset] == chunk->lines[offset - 1]) {
-    printf("   | ");
+  printf("%04d  ", offset);
+  if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1]) {
+    printf("%-4s ", "|");
   } else {
-    printf("%4d ", chunk->lines[offset]);
+    printf("%-4d ", chunk->lines[offset]);
   }
 
   uint8_t instruction = chunk->code[offset];
   switch (instruction) {
-  case OP_RETURN:
-    return simpleInstruction("OP_RETURN", offset);
   case OP_CONSTANT:
     return constantInstruction("OP_CONST", chunk, offset);
+  case OP_ADD:
+    return simpleInstruction("OP_ADD", offset);
+  case OP_SUBSTRACT:
+    return simpleInstruction("OP_SUBSTRACT", offset);
+  case OP_MULTIPLY:
+    return simpleInstruction("OP_MULTIPLY", offset);
+  case OP_DIVIDE:
+    return simpleInstruction("OP_DIVIDE", offset);
+  case OP_NEGATE:
+    return simpleInstruction("OP_NEGATE", offset);
+  case OP_RETURN:
+    return simpleInstruction("OP_RETURN", offset);
   default:
     printf("Unknown opcode %d\n", instruction);
     return offset + 1;
---
+++ src/vm.c
@@ -0,0 +1,95 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../include/common.h"
+#include "../include/compiler.h"
+#include "../include/debug.h"
+#include "../include/memory.h"
+#include "../include/vm.h"
+
+VM vm;
+
+static void resetStack() { vm.stackTop = vm.stack; }
+
+void initVM() { resetStack(); }
+
+void freeVM() {}
+
+void push(Value value) {
+  *vm.stackTop = value;
+  vm.stackTop++;
+}
+
+Value pop() {
+  vm.stackTop--;
+  return *vm.stackTop;
+}
+
+static InterpretResult run() {
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op)                                                          \
+  do {                                                                         \
+    double b = pop();                                                          \
+    double a = pop();                                                          \
+    push(a op b);                                                              \
+  } while (false)
+
+  for (;;) {
+#ifdef DEBUG_TRACE_EXECUTION
+    for (Value *slot = vm.stack; slot < vm.stackTop; slot++) {
+      printf("%-11s", " ");
+      printf("[ ");
+      printValue(*slot);
+      printf(" ]\n");
+    }
+    printf("\n");
+    disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+#endif
+
+    uint8_t instruction;
+    switch (instruction = READ_BYTE()) {
+    case OP_CONSTANT: {
+      Value constant = READ_CONSTANT();
+      push(constant);
+      break;
+    }
+    case OP_ADD: {
+      BINARY_OP(+);
+      break;
+    }
+    case OP_SUBSTRACT: {
+      BINARY_OP(-);
+      break;
+    }
+    case OP_MULTIPLY: {
+      BINARY_OP(*);
+      break;
+    }
+    case OP_DIVIDE: {
+      BINARY_OP(/);
+      break;
+    }
+
+    case OP_NEGATE: {
+      push(-pop());
+      break;
+    }
+    case OP_RETURN: {
+      printf("\n= ");
+      printValue(pop());
+      printf("\n");
+      return INTERPRET_OK;
+    }
+    }
+  }
+#undef READ_BYTE
+#undef READ_CONSTANT
+#undef BINARY_OP
+}
+
+InterpretResult interpret(const char *source) {
+  compile(source);
+  return INTERPRET_OK;
+}
